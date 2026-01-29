be2a488d66ee52585575166a04bf967c00ec7f22
Raghu Raghavan <decodewithraghu@gmail.com>
2026-01-28 17:11:43 -0800

    fix: Update PWA manifest and service worker to use SVG icons
    - Update manifest.json to use SVG icons instead of missing PNG icons
    - Update index.html apple-touch-icon references
    - Update service worker static assets cache list

--- shell/public/sw.js
+++ shell/public/sw.js
@@ -7,8 +7,7 @@ const STATIC_ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/icons/icon.svg'
 ];
 
 // Remote MFE URLs to cache
